13799e3c2c20437491236de67f56c927cedc7f0c
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-06-20 09:31:34 +0700

    Basic webserver skeleton

---
+++ go.mod
@@ -0,0 +1,8 @@
+module cowserver
+
+go 1.18
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func isFlagPassed(name string) (found bool) {
+	found = false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return
+}
+
+type Conf struct {
+	port uint
+	root string
+}
+
+func serve(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from server")
+}
+
+func getCliOptions() (options Conf) {
+	flag.UintVar(&options.port, "port", 8080, "the port to run the server on")
+	flag.StringVar(&options.root, "root", "/", "the root folder")
+	flag.Parse()
+
+	if !isFlagPassed("port") {
+		log.Println("Port unspecified, defaulting to 8080")
+	}
+
+	if !isFlagPassed("root") {
+		log.Println("Root unspecified, defaulting to '/'")
+	}
+	return
+}
+
+func main() {
+	options := getCliOptions()
+
+	http.HandleFunc(options.root, serve)
+
+	fmt.Printf("Starting server at port %d, with root '%s'\n", options.port, options.root)
+
+	portStr := fmt.Sprintf(":%d", options.port)
+
+	if err := http.ListenAndServe(portStr, nil); err != nil {
+		log.Fatal(err)
+	}
+}
